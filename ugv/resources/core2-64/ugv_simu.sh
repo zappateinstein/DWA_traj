383c97e54cc53f829e1532f7248732b55d2a1fdd
Franck <fonkouakamdemf@gmail.com>
2026-01-02 19:52:43 +0100

    okay

--- ugv/resources/core2-64/ugv_simu.sh
+++ ugv/resources/core2-64/ugv_simu.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 if [ -f /proc/xenomai/version ];then
-	EXEC=./DWA_traje_ugv_rt
+	EXEC=./DWA_traj1_ugv_rt
 else
-	EXEC=./DWA_traje_ugv_nrt
+	EXEC=./DWA_traj1_ugv_nrt
 fi
 
 $EXEC -n ugv_0 -t ugv_simu -a 127.0.0.1 -p 9000 -l /tmp -x setup_simu.xml
